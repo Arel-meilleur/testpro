506646ed9ac7d22bece9e2056a6605bb38396150
Ateufack <arelkeubou@gmail.com>
2020-08-14 11:54:15 +0100

    meilleur

--- app/Http/Controllers/DbController.php
+++ app/Http/Controllers/DbController.php
@@ -9,7 +9,7 @@ class DbController extends Controller
     public function index(){
         $req = DB::table('employee')->get();
         dd($req);
-        //Je suis le meilleur des meilleurs dsdfdsfsdf
+        //Je suis le meilleur des meilleurs dsdfdsfsdfdsfdfd
 
        
     }
